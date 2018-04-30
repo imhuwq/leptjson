846078977e5025d12c97cbd6a7ecacae77c7e864
imhuwq <imhuwq@gmail.com>
2018-04-30 17:45:02 +0800

    refactor(test): add macro to simplify error test case

--- test.cpp
+++ test.cpp
@@ -22,6 +22,14 @@ static int test_pass = 0;
 
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d");
 
+#define TEST_ERROR(error, json)\
+  do {\
+    lept_value v;\
+    v.type = LEPT_FALSE;\
+    EXPECT_EQ_INT(error, lept_parse(&v, json));\
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));\
+  } while(0)
+
 static void test_parse_null() {
   lept_value v;
   v.type = LEPT_TRUE;
@@ -44,24 +52,16 @@ static void test_parse_false() {
 }
 
 static void test_parse_expect_value() {
-  lept_value v;
-  v.type = LEPT_NULL;
-  EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, ""));
-  EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+  TEST_ERROR(LEPT_PARSE_EXPECT_VALUE, "");
+  TEST_ERROR(LEPT_PARSE_EXPECT_VALUE, " ");
 }
 
 static void test_parse_invalid_value() {
-  lept_value v;
-  v.type = LEPT_NULL;
-  EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "asd"));
-  EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+  TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "asdf");
 }
 
 static void test_parse_root_not_singular() {
-  lept_value v;
-  v.type = LEPT_NULL;
-  EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&v, "false false"));
-  EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+  TEST_ERROR(LEPT_PARSE_ROOT_NOT_SINGULAR, "false true");
 }
 
 static void test_parse() {
