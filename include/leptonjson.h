1af0e22f83dd1dc65dfedd0ceb75fb478fdd317b
imhuwq <imhuwq@gmail.com>
2018-05-01 16:59:55 +0800

    new(string): support parse string

--- include/leptonjson.h
+++ include/leptonjson.h
@@ -15,24 +15,49 @@ typedef enum {
 } lept_type;
 
 typedef struct {
-  double n;
+  union {
+    struct { char *s; size_t len; } s;
+    double n;
+  } u;
   lept_type type;
 } lept_value;
 
 typedef struct {
   const char *json;
+  char *stack;
+  size_t size, top;
 } lept_context;
 
 enum {
   LEPT_PARSE_OK = 0,
   LEPT_PARSE_EXPECT_VALUE,
   LEPT_PARSE_INVALID_VALUE,
-  LEPT_PARSE_ROOT_NOT_SINGULAR
+  LEPT_PARSE_ROOT_NOT_SINGULAR,
+  LEPT_PARSE_NUMBER_TOO_BIG,
+  LEPT_PARSE_MISS_QUOTATION_MARK,
+  LEPT_PARSE_INVALID_STRING_ESCAPE,
+  LEPT_PARSE_INVALID_STRING_CHAR
 };
+
+#define lept_init(v) do { (v)->type = LEPT_NULL; } while(0)
+
+void lept_free(lept_value *v);
 
 int lept_parse(lept_value *v, const char *json);
 
 lept_type lept_get_type(const lept_value *v);
 
+int lept_get_boolean(const lept_value *v);
+
+void lept_set_boolean(lept_value *v, int b);
+
 double lept_get_number(const lept_value *v);
+
+void lept_set_number(lept_value *v, double n);
+
+const char *lept_get_string(const lept_value *v);
+
+size_t lept_get_string_length(const lept_value *v);
+
+void lept_set_string(lept_value *v, const char *s, size_t len);
 #endif //LEPTJSON_LEPTONJSON_H
--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <cstring>
+
+using namespace std;
+
 int main() {
-  int array[2] = {1, 2};
-  int *ap = array;
-  int *bp = ap;
-  bp++;
-  std::cout << ap << " " << bp << std::endl;
-  std::cout << *ap << " " << *bp << std::endl;
+  char lhs[] = "abcdefghi";
+  char* lhsptr = lhs;
+  char *rhs = "abcdefghi";
+  char *rhs2 = "aacdefghi";
+
+  cout << sizeof(lhs) << " " << sizeof(lhsptr) << endl;
+  cout << sizeof(lhs[0]) << endl;
+  cout << (&*lhs == lhs) << endl;
+  cout << memcmp(lhs, rhs, 7) << endl;
+  cout << memcmp(lhs, rhs2, 7) << endl;
   return 0;
 }
